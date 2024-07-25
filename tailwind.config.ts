3b037e6fdae58f39dcb10cfebd880fb7d627cb7b
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-07-26 00:59:08 +0530

    resolved meteore issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,14 +18,6 @@ const config: Config = {
         grid: "grid 15s linear infinite",
       },
       keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: 0,
-          },
-        },
         marquee: {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - var(--gap)))" },
